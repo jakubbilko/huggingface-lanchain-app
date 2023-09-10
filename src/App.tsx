c2d9a27a5ace8f56c1746c07043edf83f0f78a38
Jakub Bilko <jakub.bilko@10clouds.com>
2023-09-10 13:16:33 +0200

    feat: ui initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,50 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { ChangeEvent, useRef, useState } from "react";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [image, setImage] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const handleClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e: ProgressEvent<FileReader>) => {
+      if (e.target?.result) {
+        setImage(e.target.result as string);
+      }
+    };
+
+    reader.readAsDataURL(file);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <main>
+      <h1 className="text-[100px] text-red-600 font-creepster">Horror Story Generator</h1>
+      <input
+        type="file"
+        className="hidden"
+        ref={inputRef}
+        accept=".jpg,.png"
+        onChange={handleChange}
+      />
+      <button className="text-white bg-red-600 p-4 rounded-lg" onClick={handleClick}>
+        Select image
+      </button>
+      {!!image && <img className="m-auto my-4" alt="" src={image} />}
+      {!!desc && <p className="my-4 text-white">{desc}</p>}
+    </main>
   );
 }
 
